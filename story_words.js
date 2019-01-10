700b2d56d97dbda96a00799eb7827c972b7a637f
Tfangz <tfangz@ymail.com>
2019-01-10 16:19:13 +0800

    Update story_words.js

--- story_words.js
+++ story_words.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false, args: [ '--proxy-server=165.225.116.16:80' ] });
+  const browser = await puppeteer.launch({headless: false, args: [ '--proxy-server=IP:PORT' ] });
   const page = await browser.newPage();
   await page.setViewport({width: 768, height: 1024, deviceScaleFactor: 2})
   
